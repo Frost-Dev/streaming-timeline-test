82e4db55377756fd41b81ae0d707bfbd4dd10293
Marihachi <marihachi0620@gmail.com>
2017-05-05 15:38:07 +0900

    update

--- src/api.js
+++ src/api.js
@@ -14,58 +14,62 @@ http.listen(3001, () => {
 
 // 仮のUserデータベース
 const userDb = [
-	{accessKey: 'user_a_access_key', userId: 'user_a_id'}
+	{accessKey: 'user_a_access_key', userId: 'user_a_id'},
+	{accessKey: 'user_b_access_key', userId: 'user_b_id'}
 ];
 const findUser = (accessKey) => {
-	return userDb.find(i => i.accessKey === data.accessKey);
+	return userDb.find(i => i.accessKey === accessKey);
 };
 
 ioServerWeb.sockets.on('connection', serverSocketWeb => {
-	console.log(`connect: id=${serverSocketWeb.id}`);
+	console.log(`[api/serverSocketWeb]connected. id=${serverSocketWeb.id}`);
 
 	// Web側からフォロー関係の作成の指示を受信したとき
 	serverSocketWeb.on('create-follow-test', data => {
-		console.log(`[api/create-follow-test]data: '${inspect(data)}'`);
-		if (data.applicationKey == null || data.accessKey == null) {
-			console.log('[api/create-follow-test]error: applicationKey/AccessKey are empty');
+		console.log(`[api/serverSocketWeb]on create-follow-test`);
 
+		if (data.applicationKey == null || data.accessKey == null) {
 			// Web側にエラーを返す
+			console.log(`[api/ioServerWeb]send error.`);
 			ioServerWeb.to(serverSocketWeb.id).emit('error', {message: 'applicationKey/AccessKey are empty'});
+
 			return;
 		}
 
-		console.log('[api/create-follow-test]follow: ' + data.target);
-
 		// Redisで購読
+		console.log(`[api/subscriber]subscribe user '${data.target}'.`);
 		subscriber.subscribe(`${data.target}`);
 	});
 
 	// Web側からステータス作成の指示を受信したとき
 	serverSocketWeb.on('create-status-test', data => {
-		console.log(`[api/create-status-test]data: '${inspect(data)}'`);
-		if (data.applicationKey == null || data.accessKey == null) {
-			console.log('[api/create-status-test]error: applicationKey/AccessKey are empty');
+		console.log(`[api/serverSocketWeb]on create-status-test`);
 
+		if (data.applicationKey == null || data.accessKey == null) {
 			// Web側にエラーを返す
+			console.log(`[api/ioServerWeb]send error.`);
 			ioServerWeb.to(serverSocketWeb.id).emit('error', {message: 'applicationKey/AccessKey are empty'});
+
 			return;
 		}
 
 		const userId = findUser(data.accessKey).userId;
 
 		// Redisで出版
+		console.log(`[api/publisher]publish status.`);
 		publisher.publish(userId, JSON.stringify(data));
 	});
 
 	// Redisで購読している対象からのメッセージを受信したとき
 	subscriber.on('message', (userId, data) => {
-		console.log('[api/subscriber]event: userId=' + userId + ', data=' + data);
+		console.log('[api/subscriber]on message: userId=' + userId + ', data=' + data);
 
 		// Web側にステータス情報を返す
+		console.log(`[api/ioServerWeb]send status.`);
 		ioServerWeb.to(serverSocketWeb.id).emit('status', JSON.parse(data));
 	});
 
 	serverSocketWeb.on('disconnect', () => {
-		console.log(`[api]disconnect: id=${serverSocketWeb.id}`);
+		console.log(`[api/serverSocketWeb]on disconnect: id=${serverSocketWeb.id}`);
 	});
 });
--- src/web.js
+++ src/web.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const inspect = require('util').inspect;
-const cookie = require('cookie');
-const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const app = require('express')();
 const http = require('http').Server(app);
+const getSessionFromCookieAsync = require('./helpers/get-session-from-cookie-async');
 
 const ioServer = require('socket.io');
 const ioClient = require('socket.io-client');
@@ -15,6 +14,7 @@ const ioClientApiSocket = ioClient('http://localhost:3001');
 const store = new RedisStore({db: 1});
 const sessionCookieName = 'sid'
 const sessionSecret = 'hogepiyo';
+const applicationKey = 'hoge1234';
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -27,9 +27,15 @@ app.use(session({
 	saveUninitialized: true
 }));
 
-app.get('/login', (req, res) => {
+app.get('/login/a', (req, res) => {
 	console.log('[web/session]set accessKey.');
 	req.session.AccessKey = 'user_a_access_key';
+	res.send('login.');
+});
+
+app.get('/login/b', (req, res) => {
+	console.log('[web/session]set accessKey.');
+	req.session.AccessKey = 'user_b_access_key';
 	res.send('login.');
 });
 
@@ -48,61 +54,67 @@ http.listen(3000, () => {
 });
 
 ioClientApiSocket.on('connect', () => {
-	console.log('[web/ioClientApiSocket]connected.');
-	ioServerFront.sockets.on('connection', ioServerFrontSocket => {
-		console.log(`connect: id=${ioServerFrontSocket.id}`);
-		let cookies = cookieParser.signedCookies(cookie.parse(ioServerFrontSocket.request.headers.cookie), sessionSecret);
+	console.log(`[web/ioClientApiSocket]connected. id=${ioClientApiSocket.id}`);
 
-		store.get(cookies[sessionCookieName], (err, session) => {
-			console.log(`[web/ioServerFrontSocket]fetched session: ${inspect(session)}`);
+	ioServerFront.sockets.on('connection', ioServerFrontSocket => {
+		(async () => {
+			console.log(`[web/ioServerFrontSocket]connected. id=${ioServerFrontSocket.id}`);
+
+			const session = await getSessionFromCookieAsync(ioServerFrontSocket.request.headers.cookie, sessionCookieName, sessionSecret, store);
+			console.log('[web/ioServerFrontSocket]fetched session.');
 
 			// フロント側からフォロー関係の作成の指示を受信したとき
 			ioServerFrontSocket.on('create-follow-test', data => {
-				console.log('[web/ioServerFrontSocket]send to api follow relationship');
+				console.log('[web/ioServerFrontSocket]on create-follow-test');
 
 				// API側にフォロー関係の作成を指示
 				ioClientApiSocket.emit('create-follow-test', {
 					target: data.value,
-					applicationKey: 'hoge1234',
+					applicationKey: applicationKey,
 					accessKey: session.AccessKey
 				});
 			});
 
 			// フロント側からステータスの作成の指示を受信したとき
 			ioServerFrontSocket.on('create-status-test', data => {
+				console.log('[web/ioServerFrontSocket]on create-status-test');
+
 				if (!session.AccessKey) {
 					console.log('[web/ioServerFrontSocket]error: unauthorized');
 
 					// フロント側にエラーを返す
 					ioServerFront.to(ioServerFrontSocket.id).emit('error', {message: 'unauthorized'});
+
 					return;
 				}
-
-				console.log('[web/ioServerFrontSocket]send status data to api');
 
 				// API側にステータス作成を指示
 				ioClientApiSocket.emit('create-status-test', {
 					text: data.value,
-					applicationKey: 'hoge1234',
+					applicationKey: applicationKey,
 					accessKey: session.AccessKey
 				});
 			});
 
 			// API側からステータス情報を受信したとき
 			ioClientApiSocket.on('status', data => {
+				console.log('[web/ioClientApiSocket]on status');
+
 				// フロント側にステータス情報を返す
 				ioServerFront.to(ioServerFrontSocket.id).emit('status', data);
 			});
 
 			// API側からエラーを受信したとき
 			ioClientApiSocket.on('error', data => {
+				console.log('[web/ioClientApiSocket]on error');
+
 				// フロント側にエラーを返す
 				ioServerFront.to(ioServerFrontSocket.id).emit('error', data);
 			});
-		});
 
-		ioServerFrontSocket.on('disconnect', () => {
-			console.log(`[web]disconnect: id=${ioServerFrontSocket.id}`);
-		});
+			ioServerFrontSocket.on('disconnect', () => {
+				console.log(`[web/ioServerFrontSocket]on disconnect: id=${ioServerFrontSocket.id}`);
+			});
+		})();
 	});
 });
